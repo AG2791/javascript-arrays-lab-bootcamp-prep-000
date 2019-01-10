708510ccf3bf901d6c730ed9f602e4d81297083a
AJ Guiles <ashiguiles@gmail.com>
2019-01-10 06:11:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-var kittens = [] //define your array here
+var kittens = ['Milo','Otis','Garfield'] 
 
-// Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push('Ralph');
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift('Bob');
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  return [...kittens, name]
+}
